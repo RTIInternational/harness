f2ecac544174d05d27fa47bb1187fb2aa370d0b3
Roman Ruiz-Esparza <rjruizes@ncsu.edu>
2021-10-22 14:03:57 -0400

    vue add typescript

--- jest.config.js
+++ jest.config.js
@@ -3,29 +3,44 @@ module.exports = {
     'js',
     'jsx',
     'json',
-    'vue'
+    'vue',
+    'ts',
+    'tsx'
   ],
+
   transform: {
     '^.+\\.vue$': 'vue-jest',
     '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
-    '^.+\\.jsx?$': 'babel-jest'
+    '^.+\\.jsx?$': 'babel-jest',
+    '^.+\\.tsx?$': 'ts-jest'
   },
 
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1'
   },
+
   snapshotSerializers: [
     'jest-serializer-vue'
   ],
+
   testMatch: [
     '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'
   ],
+
   testURL: 'http://localhost/',
+
   watchPlugins: [
     'jest-watch-typeahead/filename',
     'jest-watch-typeahead/testname'
   ],
+
   setupTestFrameworkScriptFile: '<rootDir>/tests/unit/setupTests.js',
   collectCoverage: true,
-  collectCoverageFrom: ['**/*.{js,vue}', '!**/node_modules/**', '!**/dist/**', '!**/docs/**']
+  collectCoverageFrom: ['**/*.{js,vue}', '!**/node_modules/**', '!**/dist/**', '!**/docs/**'],
+
+  globals: {
+    'ts-jest': {
+      babelConfig: true
+    }
+  }
 }
